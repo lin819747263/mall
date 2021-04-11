b31dd4ec04af27098738b1e8aa12c671443202e4
lin819747263 <819747263@qq.com>
2021-04-11 10:52:59 +0800

    修复问题

--- mall-common/src/main/java/com/linmsen/config/RedissonCpnfig.java
+++ mall-common/src/main/java/com/linmsen/config/RedissonCpnfig.java
@@ -5,11 +5,13 @@ import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+@Configuration
 public class RedissonCpnfig {
     @Value("${spring.redis.host}")
     private String redisHost;
@@ -17,8 +19,8 @@ public class RedissonCpnfig {
     @Value("${spring.redis.port}")
     private String redisPort;
 
-    @Value("${spring.redis.password}")
-    private String redisPwd;
+//    @Value("${spring.redis.password}")
+//    private String redisPwd;
 
     /**
      * 配置分布式锁
@@ -30,7 +32,7 @@ public class RedissonCpnfig {
 
         //单机模式
         //config.useSingleServer().setPassword("123456").setAddress("redis://8.129.113.233:3308");
-        config.useSingleServer().setPassword(redisPwd).setAddress("redis://"+redisHost+":"+redisPort);
+        config.useSingleServer().setAddress("redis://"+redisHost+":"+redisPort);
 
         //集群模式
         //config.useClusterServers()
